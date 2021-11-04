c0674d7dd0b3f1b2e8776995eb494d4ff86378df
RiazK <RiazCKhan@gmail.com>
2021-11-03 20:41:22 -0400

    Add components to App()

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from './components/navigation';
+import Profile from './components/profile';
+import TweetForm from './components/tweetForm';
+import TweetList from './components/tweetList';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Profile />
+      <TweetForm />
+      <TweetList />
     </div>
   );
 }
