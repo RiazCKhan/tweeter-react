552d663615148fc495e4068ec785d8c0f47c7965
RiazK <RiazCKhan@gmail.com>
2021-11-04 10:14:16 -0400

    Add prevent default to onClick and onSubmit

--- react-version/src/components/tweetForm.jsx
+++ react-version/src/components/tweetForm.jsx
@@ -1,12 +1,15 @@
 const TweetForm = () => {
-
   const text = "What are you humming about?"
+  
+  const PreventDefault = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder={ text }></textarea>
-        <input type="submit" value="Tweet" className="form__input" />
+      <form onSubmit={PreventDefault} method="post" action="/tweets" className="newtweet__form">
+        <textarea className="form__textarea" name="text" placeholder={text}></textarea>
+        <input onClick={PreventDefault} type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
       </form>
     </section>
