64c87f149937a2878d64e58d53e30fafb4a06cdf
RiazK <RiazCKhan@gmail.com>
2021-11-03 20:59:16 -0400

    Add tweeter content

--- react-version/src/components/navigation.jsx
+++ react-version/src/components/navigation.jsx
@@ -1,7 +1,17 @@
 const Navigation = () => {
   return (
-    <h1>I am the navigation component</h1>
-  )
+    <nav>
+      <span class="nav--text">tweeter</span>
+
+      <div class="btn btn--scroll">
+        <a href="#" class="btn__text btn--borderless" title="Compose Tweet">
+          <span class="btn--bold">Write</span> a new tweet
+          <br />
+          <i class ="btn__icon fas fa-angle-double-down"></i>
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigation;
